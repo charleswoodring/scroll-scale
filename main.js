731d217d5bebdae0c3219cd14d6a7155c7e39955
Charles Woodring <charles.s.woodring@gmail.com>
2019-01-24 12:44:11 -0600

    vertical working

--- main.js
+++ main.js
@@ -4,18 +4,19 @@ const audrey = document.getElementById("audrey")
     Add an event listener to the `document` object to listen
     for the "scroll" event.
 */
-document.addEventListener("scroll", function (event) {
-    audrey.style.width = `${window.scrollY / 3}px`;
-    console.log(event)
-    /*
+
+/*
         Adjust the width of audrey to be 1/3 the value of
         `window.scrollY`. No lower than 50px, though.
-    */
-
-
-
+*/
+document.addEventListener("scroll", function (event) {
+    audrey.style.width = `${window.scrollY / 3}px`;}
+    )
+    
     /*
         Adjust the height of audrey to be 1/4 the value of
         `window.scrollY`. No lower than 100px, though.
     */
-})
+   document.addEventListener("scroll", function (event) {
+    audrey.style.height = `${window.scrollY / 4}px`;}
+)
